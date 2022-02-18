be3a9b0a6147b5b138455155d1f5b3c4ec1354c1
CodyAaTherf <aatherfgamerplayzs@gmail.com>
2022-02-18 21:18:27 +0530

    Converting JS Object to JS String

---
+++ Instagram-Tutorials/JS/js-json-string.js
@@ -0,0 +1,10 @@
+// Converting JS Object to JS String
+
+const user = {
+    name: "April" ,
+    age: 16 ,
+    phonenumber: "0123456789" ,
+}
+
+var userData = JSON.stringify(user);
+console.log(userData);
