75a9215347b52a1595da36bbbf4496edcbba0ae8
CodyAaTherf <aatherfgamerplayzs@gmail.com>
2022-05-29 00:11:57 +0530

    added choose_max_number

--- Instagram-Tutorials/Python/number-guess.py
+++ Instagram-Tutorials/Python/number-guess.py
@@ -3,10 +3,11 @@ import random
 print("Welcome to the number guessing game!")
 
 def guess_number():
-    number = random.randint(1 , 100)
+    choose_max_number = int(input("Choose a maximum number: "))
+    number = random.randint(1 , choose_max_number)
     chances = 0
     times = int(input("How many chances do you want to guess the number?: "))
-    print("Guess the number between 1 and 100.")
+    print(f"Guess the number between 1 and {choose_max_number}.")
 
     while chances < times:
         guess = int(input("Guess the number: "))
@@ -17,12 +18,15 @@ def guess_number():
         elif guess < number:
             print("Your guess is too low.")
             chances += 1
+            print(chances)
         else:
             print("Your guess is too high.")
             chances += 1
+            print(chances)
 
     if not chances < times:
         print("You have used all your chances.")
         print("The number was" , number)
 
 guess_number()
+alive = input()
